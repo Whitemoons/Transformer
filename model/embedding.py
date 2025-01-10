4bffbc8c8438142c016ea99504fb3427300187f0
Whitemoons <alrnd01@snu.ac.kr>
2025-01-10 22:07:21 +0900

    add sub layers

--- model/embedding.py
+++ model/embedding.py
@@ -1,27 +1,33 @@
-import math
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class TokenEmbedding(nn.Module):
-    def __init__(self, vocab_size: int, d_model: int):
+    '''
+    input: [batch_size, seq_len]
+    output: [batch_size, seq_len, embedding_dim]
+    '''
+    def __init__(self, vocab_size: int, embedding_dim: int):
         super().__init__()
-        self.embedding = nn.Embedding(num_embeddings= vocab_size, embedding_dim= d_model)
+        self.embedding = nn.Embedding(num_embeddings= vocab_size, embedding_dim= embedding_dim)
     
     def forward(self, x):
         return self.embedding(x)
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, max_len: int, d_model: int):
+    '''
+    input: [batch_size, seq_len]
+    output: [1, seq_len, embedding_dim]
+    '''
+    def __init__(self, max_len: int, embedding_dim: int, device):
         super().__init__()
 
-        position = torch.arange(0, max_len).unsqueeze(1)
+        position = torch.arange(0, max_len, device=device).unsqueeze(1) # [max_len, 1]
 
-        i = torch.arange(0, d_model, 2) / d_model
+        i = torch.arange(0, embedding_dim, 2, device=device).unsqueeze(0) / embedding_dim # [1, d_model]
         exp_term = 10000 ** i
 
-        self.encoding = torch.zeros(max_len, d_model, requires_grad= False)
+        self.encoding = torch.zeros(max_len, embedding_dim, requires_grad= False, device=device)
 
         self.encoding[:, 0::2] = torch.sin(position / exp_term)
         self.encoding[:, 1::2] = torch.cos(position / exp_term)
@@ -29,17 +35,21 @@ class PositionalEncoding(nn.Module):
     def forward(self,x):
         batch_size, seq_len = x.size()
 
-        return self.encoding[:seq_len, :]
+        return self.encoding[:seq_len, :].unsqueeze(0)
 
 class TransformerEmbedding(nn.Module):
-    def __init__(self, vocab_size, max_len, d_model, drop_prob):
+    '''
+    input: [batch_size, seq_len]
+    output: [batch_size, seq_len, embedding_dim]
+    '''
+    def __init__(self, vocab_size, max_len, embedding_dim, drop_prob, device):
         super().__init__()
-        self.tokenEmbedding = TokenEmbedding(vocab_size, d_model)
-        self.positionalEncoding = PositionalEncoding(max_len, d_model)
+        self.tokenEmbedding = TokenEmbedding(vocab_size, embedding_dim)
+        self.positionalEncoding = PositionalEncoding(max_len, embedding_dim, device=device)
         self.dropOut = nn.Dropout(drop_prob)
 
     def forward(self, x):
-        # x's dim : [seq_len, d_model]
-        te = self.tokenEmbedding(x)
-        pe = self.positionalEncoding(x)
-        return self.dropOut(te+pe)
+        # x's dim : [batch_size, seq_len]
+        te = self.tokenEmbedding(x) # [batch_size, seq_len, embedding_dim]
+        pe = self.positionalEncoding(x) # [1, seq_len, embedding_dim]
+        return self.dropOut(te+pe) # broadcasting to [batch_size, seq_len, embedding_dim]
---
+++ model/subLayers.py
@@ -0,0 +1,104 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class PositionWiseScaleProductAttention(nn.Module):
+    '''
+    input:
+        Q: [batch_size, attention_head, seq_len, d_k]
+        K: [batch_size, attention_head, seq_len, d_k]
+        V: [batch_size, attention_head, seq_len, d_k]
+    output:
+        attention value: [batch_size, attention_head, seq_len, d_k]
+        attention distribution: [batch_size, attention_head, seq_len, d_k]
+    '''
+    def __init__(self, dropout=0.1):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, q, k, v, mask=None):
+        batch_size, attention_head, seq_len, d_k = q.size()
+        
+        k_T = k.transpose(2,3)
+        attention_scores = torch.matmul(q, k_T) / math.sqrt(d_k) # [batch_size, attention_head]
+
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill(mask == 0, -100000)
+
+        attention_distribution = F.softmax(attention_scores)
+        attention_distribution = self.dropout(attention_distribution)
+
+        attention_values = torch.matmul(attention_distribution, v)
+
+        return attention_values, attention_distribution
+
+class MultiHeadSelfAttention(nn.Module):
+    '''
+    input:
+        Q,K,V: [batch_size, seq_len, embedding_dim]
+
+    output:
+        output: [batch_size, seq_len, embedding_dim]
+        attention distribution: [batch_size, attention_head, seq_len, d_k] # d_k * attention_head = embedding_dim
+
+    '''
+    def __init__(self, embedding_dim, head_num, dropout=0.1):
+        super().__init__()
+        self.head_num = head_num
+        self.Q = nn.Linear(embedding_dim, embedding_dim)
+        self.K = nn.Linear(embedding_dim, embedding_dim)
+        self.V = nn.Linear(embedding_dim, embedding_dim)
+
+        self.attention = PositionWiseScaleProductAttention()
+
+        self.linear = nn.Linear(embedding_dim, embedding_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, v):
+        batch_size, seq_len, d_model = q.size()
+
+        # 1. dot product each weight matrices
+        q = self.Q(q)
+        k = self.K(k)
+        v = self.V(v)
+
+        # 2. split weight matrices
+        q = q.view([batch_size, seq_len, self.head_num, -1]).transpose(1,2) # [batch_size, head_num, seq_len, d_k]
+        k = k.view([batch_size, seq_len, self.head_num, -1]).transpose(1,2)
+        v = v.view([batch_size, seq_len, self.head_num, -1]).transpose(1,2)
+
+        # 3. calculate attention value
+        attention_values, attention_distribution = self.attention(q,k,v)
+
+        # 4. concat
+        attention_values = attention_values.transpose(1,2).reshape((batch_size, seq_len, -1))
+
+        # 5. linear
+        output = self.linear(attention_values)
+        output = self.dropout(output)
+
+        return output, attention_distribution
+
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model, hidden, dropout = 0.1):
+        super().__init__()
+
+        self.linear1 = nn.Linear(d_model, hidden)
+        self.linear2 = nn.Linear(hidden, d_model)
+
+        self.relu = nn.ReLU()
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # 1. FFN
+        output = self.linear1(x)
+        output = self.relu(output)
+        output = self.linear2(output)
+
+        output = self.dropout(output)
+
+        return output
